1bc13f71460a4abc3f7488f77162459608e8dbe6
dawnyoung <1991dongyangli@gmail.com>
2016-04-30 15:26:12 -0500

    no message

--- note-database design.sql
+++ note-database design.sql
@@ -13,13 +13,25 @@
   each table a primary key.
 - eliminate duplicate columns from the same table
 
+
 # Second normal form (2NF)
 - create tables for subsets of data that apply to multiple rows
 - create relationships between the newly created tables
+- remove partial dependencies
+- process: write each key on a separate line
+           correspond dependent attributes to the key
+           assign names of tables
 
 # Third normal form (3NF)
 - remove any columns from a table that are not dependent on the
   primary key
 - in practice, 3NF is sufficient for database normalization
+
+# Boyce-codd normal form
+- used for special 3NF
+- every determinant in the table is a candidate key
+
+# Fourth normal form (4NF)
+- dose not contain any multivalued dependencies
 
 */
