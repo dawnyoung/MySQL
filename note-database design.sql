329e232e5fbe7bbd5b32540d1a37a7cea4c3f0a7
dawnyoung <1991dongyangli@gmail.com>
2016-04-30 15:13:39 -0500

    no message

---
+++ note-database design.sql
@@ -0,0 +1,25 @@
+/*
+#############################################################
+                       Database
+#############################################################
+
+
+############Normalization
+- reduce storage requirements
+- increase database efficiency
+
+# First normal form (1NF)
+- designing a table for each set of related attributes giving 
+  each table a primary key.
+- eliminate duplicate columns from the same table
+
+# Second normal form (2NF)
+- create tables for subsets of data that apply to multiple rows
+- create relationships between the newly created tables
+
+# Third normal form (3NF)
+- remove any columns from a table that are not dependent on the
+  primary key
+- in practice, 3NF is sufficient for database normalization
+
+*/
